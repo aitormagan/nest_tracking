0eee5f6245eed3cb5a1466af35229a149610adcf
Aitor Magán <amagan@conwet.com>
2020-05-31 19:40:27 +0200

    Replace tabs by spaces

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const config = require('./config');
 
 const homebridge_api = new homebridge.HomebridgeAPI();
 const influx = new InfluxDB({
-	host: config.influx.host,
-	database: config.influx.database
+    host: config.influx.host,
+    database: config.influx.database
 });
 
 let log = function(text) {
@@ -14,25 +14,24 @@ let log = function(text) {
 log.error = log.debug = log.info = log.warn = log.warning = log.critical = log;
 
 homebridge_api.registerPlatform = function(name, type, constructor) {
-	let nest_platform = new constructor(log, {googleAuth: config.auth}, null);
-	nest_platform.accessories(function(res, err) {
-		let thermostast = res[0];
-		influx.writePoints([
-  			{
-    			measurement: 'temperature',
-    			fields: { value: thermostast.device.current_temperature },
-  			},
-  			{
-  				measurement: 'humidity',
-  				fields: { value: thermostast.device.current_humidity }
-  			}
-		]).then(() => {
-			process.exit(0);
-		}).catch(err => {
-			process.exit(1);
-		});
-
-	});
-}
+    let nest_platform = new constructor(log, {googleAuth: config.auth}, null);
+    nest_platform.accessories(function(res, err) {
+        let thermostast = res[0];
+        influx.writePoints([
+            {
+                measurement: 'temperature',
+                fields: { value: thermostast.device.current_temperature },
+            },
+            {
+                measurement: 'humidity',
+                fields: { value: thermostast.device.current_humidity }
+            }
+        ]).then(() => {
+            process.exit(0);
+        }).catch(err => {
+            process.exit(1);
+        });
+    });
+};
 
 nest(homebridge_api);
