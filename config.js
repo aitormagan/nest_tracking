ea1822a7ec3c28d420aabc76aa3d6cd62ce7408b
Aitor Magán <amagan@conwet.com>
2020-06-01 18:42:59 +0200

    Fix config indentation

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"auth": {
-	    "issueToken": "<ISSUE_TOKEN>",
-	    "cookies": "<COOKIES>",
-	    "apiKey": "<API_KEY>"
-	},
-	"influx": {
-		"host": "<INFLUXDB_HOST>",
-		"database": "<INFLUX_DATABASE>"
-	}
+    "auth": {
+        "issueToken": "<ISSUE_TOKEN>",
+        "cookies": "<COOKIES>",
+        "apiKey": "<API_KEY>"
+    },
+    "influx": {
+        "host": "<INFLUXDB_HOST>",
+        "database": "<INFLUX_DATABASE>"
+    }
 };
